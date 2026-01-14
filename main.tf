b5b0487abad7499e575e8e6138db6de0105ef697
Deepak Jain <112298618+deepakjain1409@users.noreply.github.com>
2026-01-14 20:22:40 +0530

    Initial Instance creation

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "example" {
+    count = 2
+  ami           = "ami-0ecb62995f68bb549"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-088e5ceffeadaf431"]
+  key_name = id_rsa.pem
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
