8a04734782b4890f66233cb7068f3ff188b81f71
Deepak Jain <112298618+deepakjain1409@users.noreply.github.com>
2026-01-17 10:18:28 +0530

    data block added

--- main.tf
+++ main.tf
@@ -1,24 +1,44 @@
+locals {
+  Instance_name = "ExampleInstance"
+  region        = "us-east-1"
+}
 
 terraform { 
   backend "s3" {
   bucket = "sample001-test"
   key    = "terraform.tfstate"
-  region = "us-east-1"
+  region = local.region
 }
-}
-provider "aws" {
-  region = "us-east-1"
 }
 
+provider "aws" {
+  region = local.region 
+}
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["095074107161"] # Amazon
+  
+}
 resource "aws_instance" "ExampleInstance" {
-  ami           = var.ami_id
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
 
   subnet_id              = "subnet-0ff35381a830914ad"
   vpc_security_group_ids = ["sg-088e5ceffeadaf431"]  # <-- FIX HERE
 
   tags = {
-    Name = "ExampleInstance"
+    Name = local.Instance_name
   }
 }
 
