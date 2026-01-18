6a5b699aecb034e26799f336a3f0985d2527ecf6
Deepak Jain <112298618+deepakjain1409@users.noreply.github.com>
2026-01-18 13:36:41 +0530

    updated

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,7 +6,7 @@ backend "s3" {
 }
 }
 provider "aws" {
-region = "us-east-1"
+region = "var.region"
 }
 resource "aws_security_group" "web_sg" {
     name   = "web-sg"
@@ -36,38 +36,15 @@ egress {
 tags = {
     Name = "web-sg"
 }
-}
-
-resource "aws_instance" "webserver" {
-ami           = var.ami_id
-instance_type = var.instance_type
-
-subnet_id              = module.my_vpc_module.vpc_id
-vpc_security_group_ids = [aws_security_group.web_sg.id]
-
-tags = {
-    Name = "EC2-inside-VPC"
-}
-
-
-user_data = <<-EOF
-#!/bin/bash
-apt update -y
-apt install apache2 -y
-systemctl start apache2
-systemctl enable apache2
-echo "<h1>Welcome to ExampleInstance</h1>" > /var/www/html/index.html
-EOF
 depends_on = [ aws_security_group.web_sg ]
 }
 
 module "my_vpc_module" {
 source = "./Module/VPC"
-cidr_block           = var.cidr_block
-public_subnet_cidr   = var.public_subnet_cidr
-private_subnet_cidr  = var.private_subnet_cidr
+cidr_block = var.cidr_block
+public_subnet_cidr = var.public_subnet_cidr
+private_subnet_cidr = var.private_subnet_cidr
 }
-
 
 module "EC2_module" {
 source = "./Module/EC2_Instance"
